e4036c1be5a04e9cc3d95d1b1cdf3c98a0834e94
reversebalde <yuta.fujisawa.1987@gmail.com>
2018-09-17 00:27:53 +0900

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='ssml_builder',
-    version='1.0.0',
+    version='1.0.2',
     packages=['ssml_builder'],
     url='https://github.com/Reverseblade/ssml-builder',
     license='Free',
