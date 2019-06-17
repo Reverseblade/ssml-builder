e7945696ef606fdf07ead3aa5bc7c330d7422c8d
Yuta Fujisawa <yuta.fujisawa.1987@gmail.com>
2019-06-18 02:16:11 +0900

    add tests

--- tests/test_ssml_builder.py
+++ tests/test_ssml_builder.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from ssml_builder.core import SSML
 
@@ -5,8 +7,46 @@ from ssml_builder.core import SSML
 class TestSSML(unittest.TestCase):
 
     def test_say_as(self):
-        ssml = SSML.say_as('hoge', 'spell-out')
-        self.assertEqual(ssml, '<say-as interpret-as="spell-out">hoge</say-as>')
+        ssml = SSML.say_as(value='12345', interpret_as='cardinal')
+        self.assertEqual(ssml, '<say-as interpret-as="cardinal">12345</say-as>')
+
+        ssml = SSML.say_as(value='12345', interpret_as='digits')
+        self.assertEqual(ssml, '<say-as interpret-as="digits">12345</say-as>')
+
+        ssml = SSML.say_as(value='hello', interpret_as='spell-out')
+        self.assertEqual(ssml, '<say-as interpret-as="spell-out">hello</say-as>')
+
+    def test_prosody(self):
+        ssml = SSML.prosody(value="all medium")
+        self.assertEqual(ssml, '<prosody rate="medium" pitch="medium" volume="medium">all medium</prosody>')
+
+        ssml = SSML.prosody(value="x-fast", rate='x-fast')
+        self.assertEqual(ssml, '<prosody rate="x-fast" pitch="medium" volume="medium">x-fast</prosody>')
+
+        ssml = SSML.prosody(value="slow x-high", rate='slow', pitch='x-high')
+        self.assertEqual(ssml, '<prosody rate="slow" pitch="x-high" volume="medium">slow x-high</prosody>')
+
+        ssml = SSML.prosody(value="70% low x-soft", rate='70%', pitch='low', volume='x-soft')
+        self.assertEqual(ssml, '<prosody rate="70%" pitch="low" volume="x-soft">70% low x-soft</prosody>')
+
+    def test_sub(self):
+        ssml = SSML.sub(value="Al", alias="aluminum")
+        self.assertEqual(ssml, '<sub alias="aluminum">Al</sub>')
+
+        ssml = SSML.sub(value="Mg", alias="magnesium")
+        self.assertEqual(ssml, '<sub alias="magnesium">Mg</sub>')
+
+    def test_lang(self):
+        ssml = SSML.lang(value="Paris", lang="fr-FR")
+        self.assertEqual(ssml, '<lang xml:lang="fr-FR">Paris</lang>')
+
+    def test_voice(self):
+        ssml = SSML.voice(value="I am not a real human.", name="Kendra")
+        self.assertEqual(ssml, '<voice name="Kendra">I am not a real human.</voice>')
+
+    def test_pause(self):
+        ssml = SSML.pause(time="3s")
+        self.assertEqual(ssml, '<break time="3s"/>')
 
 #
 # if __name__ == "__main__":
