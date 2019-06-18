c627022a1570ef5cdb390c8c4b1495a827dce385
Yuta Fujisawa <yuta.fujisawa.1987@gmail.com>
2019-06-19 02:13:03 +0900

    update

--- ssml_builder/core.py
+++ ssml_builder/core.py
@@ -1,72 +1,22 @@
 # -*- coding: utf-8 -*-
 
 
-class SSML:
-
-    @staticmethod
-    def say_as(value, interpret_as):
-        """
-        <say_as>
-        :param value:
-        :param interpret_as:
-        :return:
-        """
-        return '<say-as interpret-as="{}">{}</say-as>'.format(interpret_as, value)
-
-    @staticmethod
-    def prosody(value, rate='medium', pitch='medium', volume='medium'):
-        """
-        <prosody>
-        :param value:
-        :param rate:
-        :param pitch:
-        :param volume:
-        :return:
-        """
-        return '<prosody rate="{rate}" pitch="{pitch}" volume="{volume}">{value}' \
-               '</prosody>'.format(rate=rate, pitch=pitch, volume=volume, value=value)
-
-    @staticmethod
-    def sub(value, alias):
-        """
-        <sub>
-        :param alias:
-        :param value:
-        """
-        return '<sub alias="{}">{}</sub>'.format(alias, value)
-
-    @staticmethod
-    def lang(value, lang):
-        """
-        <lang>
-        :param value:
-        :param lang:
-        :param option:
-        :return:
-        """
-        return '<lang xml:lang="{}">{}</lang>'.format(lang, value)
-
-    @staticmethod
-    def voice(value, name):
-        """
-        <voice>
-        :param value:
-        :param name:
-        :return:
-        """
-        return '<voice name="{}">{}</voice>'.format(name, value)
-
-    @staticmethod
-    def pause(time):
-        """
-        <break>
-        :param time:
-        :return:
-        """
-        return '<break time="{}"/>'.format(time)
-
-
 class Speech:
+
+    VALID_INTERPRET_AS = ('characters', 'spell-out', 'cardinal', 'number',
+                          'ordinal', 'digits', 'fraction', 'unit', 'date',
+                          'time', 'telephone', 'address', 'interjection', 'expletive')
+
+    VALID_PROSODY_ATTRIBUTES = {
+        'rate': ('x-slow', 'slow', 'medium', 'fast', 'x-fast'),
+        'pitch': ('x-low', 'low', 'medium', 'high', 'x-high'),
+        'volume': ('silent', 'x-soft', 'soft', 'medium', 'loud', 'x-loud')
+    }
+
+    VALID_VOICE_NAME = ('Ivy', 'Joanna', 'Joey', 'Justin', 'Kendra', 'Kimberly',
+                        'Matthew', 'Salli', 'Nicole', 'Russell', 'Amy', 'Brian', 'Emma',
+                        'Aditi', 'Raveena', 'Hans', 'Marlene', 'Vicki', 'Conchita', 'Enrique',
+                        'Carla', 'Giorgio', 'Mizuki', 'Takumi', 'Celine', 'Lea', 'Mathieu')
 
     def __init__(self):
         self.speech = ""
@@ -78,7 +28,7 @@ class Speech:
         """
         return '<speak>{}</speak>'.format(self.speech)
 
-    def say(self, value):
+    def add_text(self, value):
         """
         add text
         :return:
@@ -86,68 +36,128 @@ class Speech:
         self.speech += value
         return self
 
-    def say_as(self, value, interpret_as):
+    def say_as(self, value, interpret_as, is_nested=False):
         """
         <say_as>
         :param value:
         :param interpret_as:
+        :param is_nested:
         :return:
         """
-        self.speech += '<say-as interpret-as="{}">{}</say-as>'.format(interpret_as, value)
+
+        if interpret_as not in self.VALID_INTERPRET_AS:
+            raise ValueError('The interpret-as provided to say_as is not valid')
+
+        ssml = '<say-as interpret-as="{interpret_as}">' \
+               '{value}</say-as>'.format(interpret_as=interpret_as, value=value)
+
+        if is_nested:
+            return ssml
+
+        self.speech += ssml
         return self
 
-    def prosody(self, value, rate='medium', pitch='medium', volume='medium'):
+    def prosody(self, value, rate='medium', pitch='medium', volume='medium', is_nested=False):
         """
         <prosody>
         :param value:
         :param rate:
         :param pitch:
         :param volume:
+        :param is_nested:
         :return:
         """
-        self.speech += '<prosody rate="{rate}" pitch="{pitch}" volume="{volume}">' \
-                       '{value}</prosody>'.format(rate=rate, pitch=pitch, volume=volume, value=value)
+
+        if rate not in self.VALID_PROSODY_ATTRIBUTES['rate']:
+            raise ValueError('The rate provided to prosody is not valid')
+
+        if pitch not in self.VALID_PROSODY_ATTRIBUTES['pitch']:
+            raise ValueError('The pitch provided to prosody is not valid')
+
+        if volume not in self.VALID_PROSODY_ATTRIBUTES['volume']:
+            raise ValueError('The volume provided to prosody is not valid')
+
+        ssml = '<prosody rate="{rate}" pitch="{pitch}" volume="{volume}">' \
+               '{value}</prosody>'.format(rate=rate, pitch=pitch, volume=volume, value=value)
+
+        if is_nested:
+            return ssml
+
+        self.speech += ssml
         return self
 
-    def sub(self, value, alias):
+    def sub(self, value, alias, is_nested=False):
         """
         <sub>
-        :param alias:
         :param value:
-        :self.speech +=:
+        :param alias:
+        :param is_nested:
+        :return:
         """
-        self.speech += '<sub alias="{}">{}</sub>'.format(alias, value)
+
+        ssml = '<sub alias="{}">{}</sub>'.format(alias, value)
+
+        if is_nested:
+            return ssml
+
+        self.speech += ssml
         return self
 
-    def lang(self, value, lang):
+    def lang(self, value, lang, is_nested=False):
         """
         <lang>
         :param value:
         :param lang:
-        :param option:
+        :param is_nested:
         :return:
         """
 
-        self.speech += '<lang xml:lang="{}">{}</lang>'.format(lang, value)
+        ssml = '<lang xml:lang="{}">{}</lang>'.format(lang, value)
+
+        if is_nested:
+            return ssml
+
+        self.speech += ssml
         return self
 
-    def voice(self, value, name):
+    def voice(self, value, name, is_nested=False):
         """
         <voice>
         :param value:
         :param name:
         :return:
         """
+
+        if name not in self.VALID_VOICE_NAME:
+            raise ValueError('The name provided to voice is not valid')
+
+        ssml = '<voice name="{}">{}</voice>'.format(name, value)
+
+        if is_nested:
+            return ssml
+
         self.speech += '<voice name="{}">{}</voice>'.format(name, value)
         return self
 
-    def pause(self, time):
+    def pause(self, time, is_nested=False):
         """
         <break>
         :param time:
+        :param is_nested:
         :return:
         """
-        self.speech += '<break time="{}"/>'.format(time)
+
+        ssml = '<break time="{}"/>'.format(time)
+
+        if is_nested:
+            return ssml
+
+        self.speech += ssml
         return self
 
-
+    def escape(self):
+        """
+        escapes any special characters that will cause SSML to be invalid
+        :return:
+        """
+        pass
--- tests/test_ssml_builder.py
+++ tests/test_ssml_builder.py
@@ -1,55 +1,60 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from ssml_builder.core import SSML
+from ssml_builder.core import Speech
 
 
-class TestSSML(unittest.TestCase):
+class TestSpeech(unittest.TestCase):
 
     def test_say_as(self):
-        ssml = SSML.say_as(value='12345', interpret_as='cardinal')
+        speech = Speech()
+        ssml = speech.say_as(value='12345', interpret_as='cardinal', is_nested=True)
         self.assertEqual(ssml, '<say-as interpret-as="cardinal">12345</say-as>')
 
-        ssml = SSML.say_as(value='12345', interpret_as='digits')
+        ssml = speech.say_as(value='12345', interpret_as='digits', is_nested=True)
         self.assertEqual(ssml, '<say-as interpret-as="digits">12345</say-as>')
 
-        ssml = SSML.say_as(value='hello', interpret_as='spell-out')
+        ssml = speech.say_as(value='hello', interpret_as='spell-out', is_nested=True)
         self.assertEqual(ssml, '<say-as interpret-as="spell-out">hello</say-as>')
 
     def test_prosody(self):
-        ssml = SSML.prosody(value="all medium")
+        speech = Speech()
+
+        ssml = speech.prosody(value="all medium", is_nested=True)
         self.assertEqual(ssml, '<prosody rate="medium" pitch="medium" volume="medium">all medium</prosody>')
 
-        ssml = SSML.prosody(value="x-fast", rate='x-fast')
+        ssml = speech.prosody(value="x-fast", rate='x-fast', is_nested=True)
         self.assertEqual(ssml, '<prosody rate="x-fast" pitch="medium" volume="medium">x-fast</prosody>')
 
-        ssml = SSML.prosody(value="slow x-high", rate='slow', pitch='x-high')
+        ssml = speech.prosody(value="slow x-high", rate='slow', pitch='x-high', is_nested=True)
         self.assertEqual(ssml, '<prosody rate="slow" pitch="x-high" volume="medium">slow x-high</prosody>')
 
-        ssml = SSML.prosody(value="70% low x-soft", rate='70%', pitch='low', volume='x-soft')
-        self.assertEqual(ssml, '<prosody rate="70%" pitch="low" volume="x-soft">70% low x-soft</prosody>')
+        # ssml = speech.prosody(value="70% low x-soft", rate='70%', pitch='low', volume='x-soft', is_nested=True)
+        # self.assertEqual(ssml, '<prosody rate="70%" pitch="low" volume="x-soft">70% low x-soft</prosody>')
 
     def test_sub(self):
-        ssml = SSML.sub(value="Al", alias="aluminum")
+        speech = Speech()
+
+        ssml = speech.sub(value="Al", alias="aluminum", is_nested=True)
         self.assertEqual(ssml, '<sub alias="aluminum">Al</sub>')
 
-        ssml = SSML.sub(value="Mg", alias="magnesium")
+        ssml = speech.sub(value="Mg", alias="magnesium", is_nested=True)
         self.assertEqual(ssml, '<sub alias="magnesium">Mg</sub>')
 
     def test_lang(self):
-        ssml = SSML.lang(value="Paris", lang="fr-FR")
+        speech = Speech()
+
+        ssml = speech.lang(value="Paris", lang="fr-FR", is_nested=True)
         self.assertEqual(ssml, '<lang xml:lang="fr-FR">Paris</lang>')
 
     def test_voice(self):
-        ssml = SSML.voice(value="I am not a real human.", name="Kendra")
+        speech = Speech()
+
+        ssml = speech.voice(value="I am not a real human.", name="Kendra", is_nested=True)
         self.assertEqual(ssml, '<voice name="Kendra">I am not a real human.</voice>')
 
     def test_pause(self):
-        ssml = SSML.pause(time="3s")
+        speech = Speech()
+
+        ssml = speech.pause(time="3s", is_nested=True)
         self.assertEqual(ssml, '<break time="3s"/>')
-
-#
-# if __name__ == "__main__":
-#     unittest.main()
-
-
